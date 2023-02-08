9fa6b8b24df242ee13b74ed7bedb51c94e4f6d5c
Ari | Backdev <ari160824@gmail.com>
2023-02-09 02:39:12 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,55 +2,92 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_COMMAND_LENGTH 100
+enum TokenType {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    INTEGER,
+    EXIT,
+    UNKNOWN
+};
 
-void add(int a, int b) {
-    printf("%d\n", a + b);
+struct Token {
+    enum TokenType type;
+    int value;
+};
+
+void get_token(char *input, struct Token *token) {
+    if (strcmp(input, "add") == 0) {
+        token->type = ADD;
+    } else if (strcmp(input, "subtract") == 0) {
+        token->type = SUBTRACT;
+    } else if (strcmp(input, "multiply") == 0) {
+        token->type = MULTIPLY;
+    } else if (strcmp(input, "divide") == 0) {
+        token->type = DIVIDE;
+    } else if (strcmp(input, "exit") == 0) {
+        token->type = EXIT;
+    } else {
+        token->value = atoi(input);
+        if (token->value == 0 && input[0] != '0') {
+            token->type = UNKNOWN;
+        } else {
+            token->type = INTEGER;
+        }
+    }
 }
 
-void subtract(int a, int b) {
-    printf("%d\n", a - b);
+int evaluate_expression(int first, int second, struct Token operation) {
+    switch (operation.type) {
+        case ADD:
+            return first + second;
+        case SUBTRACT:
+            return first - second;
+        case MULTIPLY:
+            return first * second;
+        case DIVIDE:
+            return first / second;
+        default:
+            printf("Error: Invalid operation\n");
+            exit(1);
+    }
 }
 
-void multiply(int a, int b) {
-    printf("%d\n", a * b);
-}
-
-void divide(int a, int b) {
-    printf("%d\n", a / b);
+int parse_expression(char *input, int *pos) {
+    int first = 0;
+    int second = 0;
+    struct Token operation;
+    struct Token token;
+    
+    get_token(input + *pos, &token);
+    if (token.type == INTEGER) {
+        first = token.value;
+        *pos += strlen(input + *pos) + 1;
+        get_token(input + *pos, &operation);
+        if (operation.type == ADD || operation.type == SUBTRACT ||
+            operation.type == MULTIPLY || operation.type == DIVIDE) {
+            *pos += strlen(input + *pos) + 1;
+            second = parse_expression(input, pos);
+            first = evaluate_expression(first, second, operation);
+        }
+    }
+    
+    return first;
 }
 
 int main() {
-    char input[MAX_COMMAND_LENGTH];
-    char *token;
-    int a, b;
+    char input[100];
+    int pos = 0;
+    int result = 0;
     
     while (1) {
         printf(">>> ");
-        fgets(input, MAX_COMMAND_LENGTH, stdin);
+        scanf("%[^\n]%*c", input);
         
-        token = strtok(input, " ");
-        if (strcmp(token, "exit\n") == 0) {
-            break;
-        } else if (strcmp(token, "add\n") == 0) {
-            a = atoi(strtok(NULL, " "));
-            b = atoi(strtok(NULL, " "));
-            add(a, b);
-        } else if (strcmp(token, "subtract\n") == 0) {
-            a = atoi(strtok(NULL, " "));
-            b = atoi(strtok(NULL, " "));
-            subtract(a, b);
-        } else if (strcmp(token, "multiply\n") == 0) {
-            a = atoi(strtok(NULL, " "));
-            b = atoi(strtok(NULL, " "));
-            multiply(a, b);
-        } else if (strcmp(token, "divide\n") == 0) {
-            a = atoi(strtok(NULL, " "));
-            b = atoi(strtok(NULL, " "));
-            divide(a, b);
-        } else {
-            printf("Unsupported command: %s", input);
-        }
+        pos = 0;
+        result = parse_expression(input, &pos);
+        printf("Result: %d\n", result);
     }
     
     return 0;
